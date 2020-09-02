945be04853feca09026ed8bc654ad988581fd95b
tyniacz <tyniacz@localhost.localdomain>
2020-09-02 20:33:23 +0200

    repo restructurization

--- countWordFrequency.py
+++
@@ -1,8 +0,0 @@
-def countWordFrequency(wordList):
-    wordNameandCount = {}
-    for newWord in wordList:
-      if newWord in wordNameandCount:
-        wordNameandCount[newWord] += 1     
-      else:
-        wordNameandCount[newWord] = 1
-    return wordNameandCount.items()
--- quicksort.py
+++
@@ -1,8 +0,0 @@
-def quicksort(array):
-    if len(array) <= 1:
-        return array
-    pivot = array[len(array) // 2]
-    left = [x for x in array if x < pivot]
-    middle = [x for x in array if x == pivot]
-    right = [x for x in array if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
