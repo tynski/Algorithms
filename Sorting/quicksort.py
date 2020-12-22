79d2bf65e5448e16b9263a10f9baaa0f5e7ed1d3
tynski <b.tynski@gmail.com>
2020-12-22 14:44:29 +0100

    quicksort sanity check added

--- Sorting/quicksort.py
+++ Sorting/quicksort.py
@@ -1,8 +1,11 @@
-def quicksort(array):
+def quick_sort(array):
     if len(array) <= 1:
         return array
     pivot = array[len(array) // 2]
     left = [x for x in array if x < pivot]
     middle = [x for x in array if x == pivot]
     right = [x for x in array if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+    return quick_sort(left) + middle + quick_sort(right)
+
+print(quick_sort([8,9,10,12,45,123,6,3,12,4,99]))
+print(quick_sort([54,23,123,4545,231,1,123,4,524,12312,2,233]))
