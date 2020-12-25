7f7c84e56e6b824d993b9a4e238da74c7b8f7ab8
tynski <b.tynski@gmail.com>
2020-12-25 22:08:35 +0100

    working solution without edge cases

--- FoobarWithGoogle/level2/task2/solution.py
+++ FoobarWithGoogle/level2/task2/solution.py
@@ -1,29 +1,50 @@
 def solution(l):
+    if not(l and sum(l) > 2):
+        return 0
+    l.sort()
     if sum(l) % 3 == 1:
-        l.sort()
         for i in l:
             if i % 3 == 1:
                 l.remove(i)
                 return convert(l[::-1])
+        m = None
+        for i in l:
+            if i % 3 == 2:
+                if m:
+                    l.remove(m)
+                    l.remove(i)
+                    return convert(l[::-1])
+                else:
+                    m = i
         return 0
     elif sum(l) % 3 == 2:
-        l.sort()
         for i in l:
             if i % 3 == 2:
                 l.remove(i)
                 return convert(l[::-1])
+        m = None
+        for i in l:
+            if i % 3 == 1:
+                if m:
+                    l.remove(m)
+                    l.remove(i)
+                    return convert(l[::-1])
+                else:
+                    m = i
         return 0
     else:
-        return convert(l.sort(reversed=True))
+        return convert(l[::-1])
 
 def convert(list): 
     return int("".join([str(i) for i in list]))
 
-def remove_bad_digit(l):
-    for i in l:
-        if i % 3:
-            l.remove(i)
-
-
 print(solution([3, 1, 4, 1, 5, 9]))
-    
+print(solution([3, 1, 4, 1]))
+print(solution([]))
+print(solution([1,1]))
+print(solution([2]))
+print(solution([1]))
+print(solution([2,2]))
+
+
+
