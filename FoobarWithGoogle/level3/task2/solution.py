0a291be6e8fe6e5c5262a3104467f5e91dbd125d
tynski <b.tynski@gmail.com>
2020-12-28 09:50:25 +0100

    refactoring

--- FoobarWithGoogle/level3/task2/solution.py
+++ FoobarWithGoogle/level3/task2/solution.py
@@ -1,17 +1,14 @@
 def solution(l):
     l.sort()
     result = 0
-    lucky_tuple = []
 
     for i in range(len(l)):
-        lucky_tuple = []
-        lucky_tuple.append(l[i])
+        counter = 1
         for j in range(i+1, len(l)):
             if l[j] % l[i] == 0:
-                lucky_tuple.append(l[j])
-            if len(lucky_tuple) == 3:
+                counter += 1
+            if counter == 3:
                 result += 1
-                lucky_tuple = []
-                lucky_tuple.append(l[i])
+                counter = 1
 
     return result
