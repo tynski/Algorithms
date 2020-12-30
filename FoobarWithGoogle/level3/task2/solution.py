e961bce6d66f2622425da5c655629c07965a3498
tynski <b.tynski@gmail.com>
2020-12-30 10:07:35 +0100

    dynamic programming approach added

--- FoobarWithGoogle/level3/task2/solution.py
+++ FoobarWithGoogle/level3/task2/solution.py
@@ -1,4 +1,4 @@
-def solution(l):
+def solution_brute_force(l):
     if len(l) < 3:
         return 0
     
@@ -13,3 +13,39 @@ def solution(l):
                         result += 1
 
     return result
+
+def solution(l):
+    if len(l) < 3:
+        return 0
+
+    l.sort()
+    result = 0
+    counter = [0]*len(l)
+    
+    for i in range(len(l)):
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                counter[i] += 1
+                result += counter[j] 
+
+    return result
+
+# from collections import Counter
+# from itertools import combinations
+
+# def solution(l):
+#     counts = Counter(l)  # {x: occurrences of x}
+#     divisors = Counter()        # {x: distinct proper divisors of x}
+#     multiples = Counter()       # {x: distinct proper multiples of x}
+#     for x, y in combinations(sorted(counts), 2):
+#         if y % x == 0:
+#             divisors[y] += 1
+#             multiples[x] += 1
+#     result = 0
+#     for x, n in counts.items():
+#         result += divisors[x] * multiples[x]
+#         if n >= 2:
+#             result += divisors[x] + multiples[x]
+#             if n >= 3:
+#                 result += 1
+#     return result
