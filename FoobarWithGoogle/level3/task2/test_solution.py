86ec66c7e755dc3a6a080757223dbe769a235152
tynski <b.tynski@gmail.com>
2020-12-30 10:02:16 +0100

    tests added

--- FoobarWithGoogle/level3/task2/test_solution.py
+++ FoobarWithGoogle/level3/task2/test_solution.py
@@ -7,4 +7,10 @@ def test_solution():
 
     assert solution([2,7,8,15,21,24,65,80]) == 2
 
+    assert solution([1, 1, 1, 1]) == 4
+
+    assert solution([1, 1]) == 0
+
+    assert solution([68,234,112,32, 13, 20]) == 0
+
 test_solution()
