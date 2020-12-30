558ef9a7484711c48e737dd4437fa098cfa490f7
tynski <b.tynski@gmail.com>
2020-12-30 10:09:29 +0100

    tests updated

--- FoobarWithGoogle/level3/task2/test_solution.py
+++ FoobarWithGoogle/level3/task2/test_solution.py
@@ -1,16 +1,18 @@
 from solution import solution
 
+
 def test_solution():
-    assert  solution([1, 2, 3, 4, 5, 6]) == 3
+    assert solution([1, 2, 3, 4, 5, 6]) == 3
 
     assert solution([1, 1, 1]) == 1
 
-    assert solution([2,7,8,15,21,24,65,80]) == 2
+    assert solution([2, 7, 8, 15, 21, 24, 65, 80]) == 2
 
     assert solution([1, 1, 1, 1]) == 4
 
     assert solution([1, 1]) == 0
 
-    assert solution([68,234,112,32, 13, 20]) == 0
+    assert solution([68, 234, 112, 32, 13, 20]) == 0
+
 
 test_solution()
