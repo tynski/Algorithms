d9f4efe9964dcf1a49833b0e3e74743bc2bc5339
tynski <b.tynski@gmail.com>
2021-01-05 11:11:47 +0100

    level4 task1 initialized

---
+++ FoobarWithGoogle/level4/task1/test_solution.py
@@ -0,0 +1,6 @@
+#Test 3 is returning 0
+from solution import solution
+
+def test_solution():
+    assert solution([0], [3], [[0, 7, 0, 0], [0, 0, 6, 0], [0, 0, 0, 8], [9, 0, 0, 0]]) == 6
+    assert solution([0, 1], [4, 5], [[0, 0, 4, 6, 0, 0], [0, 0, 5, 2, 0, 0], [0, 0, 0, 0, 4, 4], [0, 0, 0, 0, 6, 6], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]) == 16
