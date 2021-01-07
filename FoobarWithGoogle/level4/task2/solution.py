cf4b405e26970e2a15d00500ed3a904a5df4b759
tynski <b.tynski@gmail.com>
2021-01-07 17:45:11 +0100

    task2 added

---
+++ FoobarWithGoogle/level4/task2/solution.py
@@ -0,0 +1,1 @@
+def solution(times, times_limit):
---
+++ FoobarWithGoogle/level4/task2/test_solution.py
@@ -0,0 +1,5 @@
+import solution from solution
+
+def test_solution():
+    assert solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1) == [1,2]
+    assert solution(solution([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3)) == [0,1]
