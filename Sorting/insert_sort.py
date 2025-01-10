b166678a823da09199273936f8756ba1e9b16d7e
tynski <b.tynski@gmail.com>
2025-01-10 17:22:19 +0100

    rename

--- Sorting/insert_sort.py
+++ Sorting/insert_sort.py
@@ -3,7 +3,7 @@ def insertSort(array):
     for j in range(1, N):
         key = array[j]
         i = j - 1
-        while array[i] > key:
+        while i => 0 and array[i] > key:
             array[i + 1] = array[i]
             i -= 1
         array[i + 1] = key
