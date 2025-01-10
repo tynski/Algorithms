03e13bc2df5ddf1a1363b873cd9d657a0936d1bb
tynski <b.tynski@gmail.com>
2025-01-10 17:35:32 +0100

    update

--- Sorting/insert_sort.py
+++ Sorting/insert_sort.py
@@ -1,9 +1,9 @@
-def insertSort(array):
+def insert_sort(array):
     N = len(array)
     for j in range(1, N):
         key = array[j]
         i = j - 1
-        while i => 0 and array[i] > key:
+        while i >= 0 and array[i] > key:
             array[i + 1] = array[i]
             i -= 1
         array[i + 1] = key
