1bbb65721ad8f4005f48d35def5ba643f4bb808d
tynski <b.tynski@gmail.com>
2025-01-13 17:43:50 +0100

    palindrome_index

---
+++ Utilities/palindrome.py
@@ -0,0 +1,8 @@
+def palindrome_index(word):
+    n = len(word)
+    for i in range(n//2+1):
+        if word[i] != word[n-1-i]:
+            if word[i] == word[n-2-i] and word[i+1] == word[n-3-i]:
+                return n-1-i
+            return i
+    return -1
